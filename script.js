c216b627c0719348d80f831b67caf33ddab31b3a
Jędrzej <76038009+Zeszen@users.noreply.github.com>
2021-05-04 00:59:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function leftsidebar() 
-{
-    document.getElementById("leftsidemenu").style.display = "block";
+//Get the button
+var mybutton = document.getElementById("goup");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
