a2527751328999a63bcadadc8f79c87977fed71a
Jędrzej <76038009+Zeszen@users.noreply.github.com>
2021-05-15 16:05:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 //Get the button
-var mybutton = document.getElementById("goup");
-const chk = document.getElementById('chk');
-const soom = document.getElementById('soom');
-const dgebi = document.getElementById;
-const dgelcn = document.getElementsByClassName;
+let mybutton = document.getElementById("goup");
+let allStickButton = document.getElementById("allstick");
+let menuStickButton = document.getElementById("menustick");
+let header = document.getElementById("header");
+let logo = document.getElementById("logo");
+let ulSmallBox = document.getElementById("ulSmallBox");
+const chk = document.getElementById("chk");
+const soom = document.getElementById("soom");
+const menu = document.getElementById("logoIconsMenu");
+const color = document.getElementById("logoIconsColor");
+const smallMenuChk = document.getElementById("smallmenu");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
+    allStickButton.style.display = "block";
+    menuStickButton.style.display = "block";
   } else {
     mybutton.style.display = "none";
+    allStickButton.style.display = "none";
+    menuStickButton.style.display = "none";
   }
 }
 
@@ -22,6 +34,15 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+  header.classList.toggle("dark");
+  logo.classList.toggle("dark");
+  menu.classList.toggle("dark");
+  color.classList.toggle("dark");
+  dgebi("small").classList.toggle("dark");
+});
+
+smallMenuChk.addEventListener("change", () => {
+  ulSmallBox.classList.toggle("visible");
 });
